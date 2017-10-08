b333f371efdeddd30e3448782a7985b98dad1bb1
Jcao <jackysfbay@mail.com>
2017-10-08 18:46:29 +0800

    create project

---
+++ Blog/src/main/java/com/blog/Blog/App.java
@@ -0,0 +1,13 @@
+package com.blog.Blog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
